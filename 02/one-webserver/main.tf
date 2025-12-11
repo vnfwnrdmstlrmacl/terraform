080af6912ddf1d31c7c6c8acc3e315092b0ae5f7
vnfwnrdmstlrmacl <27dlwldud27@gmail.com>
2025-12-11 17:05:09 +0900

    3 commit

---
+++ 02/one-webserver/main.tf
@@ -0,0 +1,72 @@
+#################
+# 1. provider
+# 2. EC2 instance 생성(user_data)
+# * SG(8080)
+# * EC2 생성
+# 
+#################
+
+#
+# 1. terraform/provider
+#
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+}
+
+#
+# 2. resources - EC2 instance 생성(user_data)
+# 
+
+# SG 생성
+
+resource "aws_security_group" "allow_8080" {
+  name        = "allow_8080"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_8080"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_8080_http" {
+  security_group_id = aws_security_group.allow_8080.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_8080.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+# EC2 생성
+
+
+resource "aws_instance" "myinstance" {
+  ami                    = ami-0f5fcdfbd140e4ab7
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_8080.id]
+
+  user_data_replace_on_change = true
+  user_data                   = <<EOF
+#!/bin/bash
+echo "Hello World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
+
+  tags = {
+    Name = "my-first-instance"
+  }
+}
+
